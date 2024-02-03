f497ab46dc81aaa65c8c307dc09d67d75e77285a
southerntw <fauzanmansur.a@gmail.com>
2024-02-03 14:18:29 +0800

    fix: fix logo missing in dynamic routes

--- app/(main)/_components/logo.tsx
+++ app/(main)/_components/logo.tsx
@@ -5,7 +5,7 @@ const dancingScript = Dancing_Script({ subsets: ["latin"] });
 export const Logo = () => {
   return (
     <div className="flex lg:flex-row gap-2 lg:gap-0">
-      <img src="./assets/logo.png" className="h-12 w-12 lg:mx-0 mt-2 lg:mt-0" />
+      <img src="/assets/logo.png" className="h-12 w-12 lg:mx-0 mt-2 lg:mt-0" />
       <p className="w-20 text-white leading-tight my-auto lg:ml-5 pt-2 lg:pt-0">
         Desa <b>Erelembang</b>
       </p>
